bd473259d2e78b80753e3fbf0d3d87314042ffaa
sjk5766 <wjdrnr6662@gmail.com>
2024-11-05 09:38:25 +0900

    feat: Todo 섹션 추가

---
+++ todo/components/TodoSection.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+const style = {
+  width: 100,
+  height: 30,
+};
+
+export function TodoSection() {
+  return (
+    <View style={style}>
+      <Text>Todo Section</Text>
+    </View>
+  );
+}
